0588118e944ff5bf83353e54865cad3a28fcc881
mamarzolla <marcomrzll@gmail.com>
2025-02-10 19:05:50 +0100

    finto CRUD

--- src/models/categoria.ts
+++ src/models/categoria.ts
@@ -1,6 +1,15 @@
 import  mongoose from "mongoose";
 
-const categortiaSchema = new mongoose.Schema({
+interface ICategoria  {
+    titolo?:string  //metto l'opzione di inserimento titolo o meno
+    sottotitolo: string,
+    descrizione?: string,   //metto l'opzione di inserimento descrizione o meno
+    dataCreazione?: Date, //metto l'opzione di inserimento data di creazione o meno
+    attiva?: boolean //metto l'opzione di inserimento attiva o meno
+
+}// Andiamo a creare un interfaccia per la categoria come per angular
+
+const categortiaSchema = new mongoose.Schema<ICategoria>({  // andiamo ad aggiungere il generico per la categoria per una forma piu correta di scrittura
     titolo : String, //classe che identifica una stringa se mettiamo con la maiuscola , mongoose la vuole con la maiuscola
     sottotitolo:{
         type: String,
@@ -14,6 +23,6 @@ const categortiaSchema = new mongoose.Schema({
 });
 
 // creiamo il modello
-export const Categoria = mongoose.model('Categoria', categortiaSchema, "categoria"); // il modello è la rappresentazione della collection in mongoose
+export const Categoria = mongoose.model<ICategoria>('Categoria', categortiaSchema, "categoria"); // il modello è la rappresentazione della collection in mongoose
 // i parametri sono il nome interno con cui definiamo la classe e lo schema di accoppiamento e infine definiamo il nome della collection nel db 
 //questo è un lavoro che dobbiamo fare ogni volta che dobbiamo farte delle scritture nel db
